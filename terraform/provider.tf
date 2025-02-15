c0a51c1271e95b16e2e208153776825dbb16c671
gilberto.laus@gmail.com <gilberto.laus@gmail.com>
2025-02-15 09:47:03 -0300

    chage provider.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -13,7 +13,8 @@ provider "aws" {
   secret_key                  = "test"
   skip_credentials_validation = true         
   skip_metadata_api_check     = true
-  s3_use_path_style           = true         
+  s3_use_path_style           = true
+  skip_requesting_account_id  = true
 
   endpoints {
     dynamodb = "http://localhost:4566"
