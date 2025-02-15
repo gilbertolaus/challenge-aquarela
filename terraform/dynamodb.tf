7cd2357d7dd8796a682e34276b88ad74d8aa2c46
gilberto.laus@gmail.com <gilberto.laus@gmail.com>
2025-02-15 11:06:42 -0300

    testing

--- terraform/dynamodb.tf
+++ terraform/dynamodb.tf
@@ -1,14 +1,8 @@
 # Tabela DynamoDB
-data "aws_dynamodb_table" "existing" {
-  name = "devsecops-table"
-}
-
 resource "aws_dynamodb_table" "devsecops_table" {
-  count        = data.aws_dynamodb_table.existing.id != "" ? 0 : 1
   name         = "devsecops-table"
   billing_mode = "PAY_PER_REQUEST"
   hash_key     = "id"
-}
 
   attribute {
     name = "id"
--- terraform/s3.tf
+++ terraform/s3.tf
@@ -1,11 +1,6 @@
-data "aws_s3_bucket" "existing" {
-  bucket = "meu-bucket-publico-devsecops"
-}
-
+# Bucket S3 público
 resource "aws_s3_bucket" "public_bucket" {
   bucket = "meu-bucket-publico-devsecops"
-
-  count = length(data.aws_s3_bucket.existing.id) == 0 ? 1 : 0
 }
 
 # ACL para acesso público
